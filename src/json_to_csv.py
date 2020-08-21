9c93227a38107a0cd2f945f0ab2328568fbe3588
Miguel Jiménez Gomis <jimenezgomismiguel@gmail.com>
2020-08-21 14:34:13 +0100

    no se

--- src/json_to_csv.py
+++ src/json_to_csv.py
@@ -17,6 +17,12 @@ with open('Scrapped_Data_ZYL.json') as json_file:
     data.update(data4)
 
 #Clean json file
+#'name':None, 'description': None, 'Developer':None, 'Publisher':None, 'Playable On':None, 'Genres':None, 'NA':None, 'EU':None, 'JP':None, 'Additional Content':None, 'Single-Player': None, 'Speedrun':None, 'Retirement':           None, 'Platform':None}
+dict_keys = {'name':None, 'description':None, 'Playable On':None, 'Genres':None, 'NA':None, 'EU':None, 'JP':None,
+       'Additional Content':None, 'Single-Player':None, 'Speedrun':None, 'Retirement':None,
+       'Platform':None, 'Type':None, 'Main Game':None, 'Multi-Player':None, 'Rating':None, 'Platform N':None,
+       'Publishers':None, 'Developers':None}
+
 temp_data = {}
 for key,game in data.items():
     if "Genre" in game:
@@ -26,23 +32,27 @@ for key,game in data.items():
     if "Developer" in game:
         game['Developers'] = game.pop('Developer')
     temp_data[key] = game
-data = temp_data
+#data = temp_data
+dict_keys.update(temp_data)
 del(temp_data)
 data_file = open('data_file.csv', 'w')
+
+
 
 #Create the csv writer object
 csv_writer = csv.writer(data_file)
 
+
 #Write header data first
 #The first minimun value is 22501 but, it can be change to 0 because
 #all games will have the same headers
-header = data["22654"].keys()
+header = dict_keys["22654"].keys()
 csv_writer.writerow(header)
 
-for game_id in data:
+for game_id in dict_keys:
     try:
         #Writing data of CSV file
-        csv_writer.writerow(data[game_id].values())
+        csv_writer.writerow(dict_keys[game_id].values())
     except Exception as e:
         pass
 
